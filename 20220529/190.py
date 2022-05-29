06d00cf002b00976c8958eb109e037c33dbdc272
Harold-Y <williamdlye@outlook.com>
2022-05-29 15:01:07 -0500

    yye1

---
+++ 20220529/190.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        ten = int(str(n), 2)
+        pass
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseBits(11111111111111111111111111111101))
