bfccc4eec4cfad83ce8d9a11447bf813dc497ed8
VirtualOilCake <34388992+VirtualOilCake@users.noreply.github.com>
2022-05-29 15:00:45 -0500

    Update 193.sh

--- 20220529/193.sh
+++ 20220529/193.sh
@@ -1,1 +1,10 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
+find_phone_number() {
+    input="file.txt"
+    while read -r line; 
+    do 
+    echo "$line"
+    done <"$input"
+}
+
+find_phone_number
