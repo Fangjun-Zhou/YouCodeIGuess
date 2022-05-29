0404997c660167cab56ad04087e4cb6c9ff643f1
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-05-29 15:00:58 -0500

    Update

---
+++ 20220529/155.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value, prev, next):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+
+class MinStack(object):
+
+    def __init__(self):
+        self.head = None
+        self.minNode = None
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if self.head == None:
+            self.head = Node(val, None, None)
+
+        newNode = Node(val, None, self.head)
+        self.head.prev = newNode
+        self.head = newNode
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+
+    def top(self):
+        """
+        :rtype: int
+        """
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
